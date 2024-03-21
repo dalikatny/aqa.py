78e560a8b424483e9154fa901e881e64e40f6194
dalikatny <dalikatny@icloud.com>
2024-03-21 17:24:07 +0300

    Сменил метод на click и изсенил локатор

--- lesson_5/task_5_CSSstyle.py
+++ lesson_5/task_5_CSSstyle.py
@@ -3,17 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 #зайти на site
 driver.get("http://uitestingplayground.com/classattr")
 #три раза кликните на кнопку .
-search_input=driver.find_element(By.CSS_SELECTOR,'.btn.class1.btn-primary.btn-test')
+search_input=driver.find_element(By.CSS_SELECTOR,'button.btn.class1')
 sleep(2)
-search_input.send_keys(Keys.ENTER)
+search_input.click()
 sleep(2)
-search_input.send_keys(Keys.ENTER)
+search_input.click()
 sleep(2)
-search_input.send_keys(Keys.ENTER)
+search_input.click()
 sleep(2)
