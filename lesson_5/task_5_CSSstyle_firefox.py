9995a84d76455458bf6ce43e3df0e6f0a723ef33
dalikatny <dalikatny@icloud.com>
2024-03-27 12:41:49 +0300

    нашел  нужный локатор для синей кнопки

--- lesson_5/task_5_CSSstyle_firefox.py
+++ lesson_5/task_5_CSSstyle_firefox.py
@@ -7,7 +7,7 @@ driver = webdriver.Firefox(service=service)
 #зайти на site
 driver.get("http://uitestingplayground.com/classattr")
 #три раза кликните на кнопку .
-search_button=driver.find_element(By.CSS_SELECTOR,'button.btn.class3')
+search_button=driver.find_element(By.CSS_SELECTOR,'btn.btn-primary')
 search_button.click()
 search_button.click()
 search_button.click()
